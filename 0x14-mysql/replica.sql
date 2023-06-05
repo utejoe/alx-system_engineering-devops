0b365588eba4ff1db2a1293c597d36652672574d
Uyi Obazee <utejoe.ju@gmail.com>
2023-06-05 09:00:58 +0100

    committing tasks

--- 0x14-mysql/replica.sql
+++
@@ -1,4 +0,0 @@
--- Create replica user
-CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'password';
-GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
-GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
